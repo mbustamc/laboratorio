bc80216137dc009807420121505ad2ce83d7a9be
Marco A. Bustamante <mbustamc@gmail.com>
2025-07-30 23:47:04 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -34,7 +34,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_ENGINE'] = engine
-    app.config.from_object(Config)
+    #app.config.from_object(Config)
 
     db.init_app(app)
 
